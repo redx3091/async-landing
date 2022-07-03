d8b97a86c1f94d6badf12518752de3a5dc3bf14c
Christian A <christiana@iMac-de-Christian.local>
2022-07-03 18:41:51 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,45 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCyD59CI7beJDU493glZpxgA&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ee49256eecmsh635db2ed1bed4cep109817jsn204ff3e09465',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+(async ()=> {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0.4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
+// fetch(API, options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
